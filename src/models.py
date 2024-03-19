a9d0d78622a8d837dabbaaaa811ce50706640000
Lucía Pacheco Daldés <150815101+Emerydith@users.noreply.github.com>
2024-03-19 18:52:58 +0000

    commit

--- src/models.py
+++ src/models.py
@@ -7,26 +7,35 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class Follower(Base):
+    __tablename__ = 'follower'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    follower_name = Column(String(250))
+    user_id = Column(Integer, ForeignKey('user.id'))
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Media(Base):
+    __tablename__ = 'media'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    url = Column(String(250))
+    type = Column(String(250))
+    post_id = Column(Integer, ForeignKey('post.id'))
 
-    def to_dict(self):
-        return {}
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String(250))
+    post_id = Column(Integer, ForeignKey('post.id'))
+    user_id = Column(Integer, ForeignKey('user.id'))
+
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    media = relationship(Media)
+    comment = relationship(Comment)
+
+
+
 
 ## Draw from SQLAlchemy base
 try:
@@ -35,3 +44,6 @@ try:
 except Exception as e:
     print("There was a problem genering the diagram")
     raise e
+
+    #def to_dict(self):
+     #  return {}
