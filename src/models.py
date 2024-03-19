7bcfe6f1fbb3d93c7988efd378ae8ac7b54b4fc3
Lucía Pacheco Daldés <150815101+Emerydith@users.noreply.github.com>
2024-03-19 19:21:07 +0000

    commit

--- src/models.py
+++ src/models.py
@@ -1,23 +1,34 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+import enum
+from sqlalchemy import Column, ForeignKey, Integer, Table, String, Enum
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
+class MyEnum(enum.Enum):
+    video = 1
+    photo = 2
+
+follower_user = Table('follower_user', Base.metadata,
+    Column('follower_id', Integer, ForeignKey('follower.id')),
+    Column('user_id', Integer, ForeignKey('user.id')))
+
 class Follower(Base):
     __tablename__ = 'follower'
     id = Column(Integer, primary_key=True)
     follower_name = Column(String(250))
     user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship("User", secondary=follower_user, back_populates="follower")
+    
 
 class Media(Base):
     __tablename__ = 'media'
     id = Column(Integer, primary_key=True)
     url = Column(String(250))
-    type = Column(String(250))
+    type = Column(Enum(MyEnum))
     post_id = Column(Integer, ForeignKey('post.id'))
 
 class Comment(Base):
@@ -34,6 +45,14 @@ class Post(Base):
     media = relationship(Media)
     comment = relationship(Comment)
 
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(250))
+    firstname = Column(String(250))
+    lastname = Column(String(250))
+    email = Column(String(250))
+    follower = relationship("Follower", secondary=follower_user, back_populates="user")
 
 
 
